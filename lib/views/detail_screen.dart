7a7cdcbfb1b635dc8dfa090d35541b7026e0aebc
khduy <duyhk1223@gmail.com>
2021-03-04 11:28:33 +0700

    save image

--- lib/views/detail_screen.dart
+++ lib/views/detail_screen.dart
@@ -18,113 +18,117 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: AlignmentDirectional.bottomEnd,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Hero(
-              transitionOnUserGestures: true,
-              tag: widget.indexPhoto,
-              child: CachedNetworkImage(
-                imageUrl: widget.photo.scr.portrait,
-                fit: BoxFit.cover,
+      body: Builder(
+        builder: (context) => Stack(
+          alignment: AlignmentDirectional.topEnd,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Hero(
+                transitionOnUserGestures: true,
+                tag: widget.indexPhoto,
+                child: CachedNetworkImage(
+                  imageUrl: widget.photo.scr.portrait,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 5, right: 3),
-            child: Text(
-              widget.photo.photographer,
-              style: TextStyle(color: Colors.white, fontSize: 13, fontWeight: FontWeight.bold),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 5, right: 3),
+                child: Text(
+                  widget.photo.photographer,
+                  style: TextStyle(color: Colors.white, fontSize: 13, fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
-          ),
-          Container(
-            //color: Colors.blue,
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            alignment: Alignment.bottomCenter,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                InkWell(
-                  child: Container(
-                    alignment: Alignment.center,
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.black26,
-                      border: Border.all(width: 1, color: Colors.white30),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'Save photo',
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white70),
-                        ),
-                        Text(
-                          'Photo will be saved to gallery',
-                          style: TextStyle(fontSize: 11, color: Colors.white60),
-                        )
-                      ],
-                    ),
-                  ),
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => Dialog(
-                        child: Container(
-                          height: 60,
-                          width: 100,
-                          child: Row(
-                            children: [
-                              Container(
-                                height: 50,
-                                width: 50,
-                                padding: EdgeInsets.all(5),
-                                child: CircularProgressIndicator(),
-                              ),
-                              Text('Saving...'),
-                            ],
-                          ),
-                        ),
+            Container(
+              //color: Colors.blue,
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  InkWell(
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.black26,
+                        border: Border.all(width: 1, color: Colors.white30),
                       ),
-                      barrierDismissible: false,
-                    );
-                    _saveNetworkImage();
-                  },
-                ),
-                SizedBox(height: 5),
-                InkWell(
-                  onTap: () => Navigator.pop(context),
-                  child: Text(
-                    'Cancel',
-                    style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Colors.white70,
-                        fontWeight: FontWeight.w500),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Save photo',
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white70),
+                          ),
+                          Text(
+                            'Photo will be saved to gallery',
+                            style: TextStyle(fontSize: 11, color: Colors.white60),
+                          ),
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      //todo: handle permission
+                      _saveNetworkImage(context);
+                    },
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                )
-              ],
-            ),
-          )
-        ],
+                  SizedBox(height: 5),
+                  InkWell(
+                    onTap: () => Navigator.pop(context),
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.white70,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 
-  void _saveNetworkImage() async {
-    GallerySaver.saveImage(widget.photo.scr.original).then((bool success) {
-      setState(() {
-        Navigator.pop(context);
-      });
+  void _saveNetworkImage(context) async {
+    showDialog(
+      context: context,
+      builder: (_) => Dialog(
+        child: Container(
+          height: 60,
+          width: 100,
+          child: Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                padding: EdgeInsets.all(5),
+                child: CircularProgressIndicator(),
+              ),
+              Text('Saving...'),
+            ],
+          ),
+        ),
+      ),
+      barrierDismissible: false,
+    );
+    await GallerySaver.saveImage(widget.photo.scr.original).then((bool success) {
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text('Saved')));
+      Navigator.pop(context);
     });
   }
 }
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -144,6 +144,7 @@ class _HomeState extends State<Home> {
                       getTrendingPhoto();
                     });
                   }
+                  return isLoading;
                 },
                 child: Expanded(
                   child: staggeredPhotoGrid(photos, context),
